d7fbe225973f3f7c0f69a21cbaf925e5e9aac675
woodyZootopia <piequalsabout314159@gmail.com>
2023-02-08 21:08:50 +0900

    mkdir before linking

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ ln -s $SCRIPTHOME/.latexmkrc ~/.latexmkrc
 ln -s $SCRIPTHOME/.zshrc     ~/.zshrc
 ln -s $SCRIPTHOME/.fzf.bash  ~/.fzf.bash
 ln -s $SCRIPTHOME/.fzf.zsh   ~/.fzf.zsh
-ln -s $SCRIPTHOME/.config/kitty/kitty.conf   ~/.config/kitty/kitty.conf
 ln -s $SCRIPTHOME/.config/starship.toml   ~/.config/starship.toml
-ln -s $SCRIPTHOME/.config/fontconfig/conf.d ~/.config/fontconfig/conf.d
+mkdir -p ~/.config/kitty; ln -s $SCRIPTHOME/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
+# ln -s $SCRIPTHOME/.config/fontconfig/conf.d ~/.config/fontconfig/conf.d
 # ln -s $SCRIPTHOME/.w3m ~/.w3m
 # ln -s $SCRIPTHOME/.config/libskk ~/.config/libskk/
 # ln -s $SCRIPTHOME/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
