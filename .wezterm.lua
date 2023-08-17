d88d6ac332f457028743fc0b2dffdaf31f6f1f67
woodyZootopia <piequalsabout314159@gmail.com>
2023-08-17 12:40:52 +0900

    Update .wezterm.lua Font settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-        config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -23,14 +23,14 @@ iceberg.selection_bg = 'gray'
 
 config.color_schemes = {
     ['My Iceberg'] = iceberg,
-  }
+}
 config.color_scheme = 'My Iceberg'
 
 config.macos_forward_to_ime_modifier_mask = "SHIFT|CTRL"
 
 config.use_ime = true
 
-config.font = wezterm.font_with_fallback({'JetBrains Mono', "Osaka"})
+config.font = wezterm.font_with_fallback({ "JetBrains Mono", "Hiragino Kaku Gothic Pro" })
 
 config.font_size = 13
 
