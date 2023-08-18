9a59a3f160aabc900ffed40672e5065c56a59c18
woodyZootopia <piequalsabout314159@gmail.com>
2023-08-18 11:22:59 +0900

    Add wezterm mappings

--- .wezterm.lua
+++ .wezterm.lua
@@ -33,4 +33,31 @@ config.font = wezterm.font_with_fallback({ "JetBrains Mono", "Hiragino Maru Goth
 config.font_size = 13
 
 config.tab_bar_at_bottom = true
+
+local act = wezterm.action
+config.keys = {
+    { key = 'Tab',   mods = 'SHIFT|CTRL', action = act.ActivateTabRelative(-1) },
+    { key = 'Enter', mods = 'ALT',        action = act.ToggleFullScreen },
+    { key = '\"',    mods = 'SHIFT|CTRL', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = '%',     mods = 'SHIFT|CTRL', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = ')',     mods = 'ALT',        action = act.ResetFontSize },
+    { key = '+',     mods = 'ALT',        action = act.IncreaseFontSize },
+    { key = '-',     mods = 'ALT',        action = act.DecreaseFontSize },
+    { key = '0',     mods = 'ALT',        action = act.ResetFontSize },
+    { key = '1',     mods = 'ALT',        action = act.ActivateTab(0) },
+    { key = '2',     mods = 'ALT',        action = act.ActivateTab(1) },
+    { key = '3',     mods = 'ALT',        action = act.ActivateTab(2) },
+    { key = '4',     mods = 'ALT',        action = act.ActivateTab(3) },
+    { key = '5',     mods = 'ALT',        action = act.ActivateTab(4) },
+    { key = '6',     mods = 'ALT',        action = act.ActivateTab(5) },
+    { key = '7',     mods = 'ALT',        action = act.ActivateTab(6) },
+    { key = '8',     mods = 'ALT',        action = act.ActivateTab(7) },
+    { key = '9',     mods = 'ALT',        action = act.ActivateTab(-1) },
+    { key = 't',     mods = 'ALT',        action = act.SpawnTab 'CurrentPaneDomain' },
+    { key = 'h',     mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Left' },
+    { key = 'l',     mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Right' },
+    { key = 'k',     mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Up' },
+    { key = 'j',     mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Down' },
+}
+
 return config
