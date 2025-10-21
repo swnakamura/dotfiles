981107468ecbcb9ea74d64fae2801abd14d27adb
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-10-21 15:16:41 -0700

    feat(fish): add hold project python aliases
    Add load_hold_commands function to config.fish, providing aliases for
    Python interpreters in various hold project submodules (hamer,
    sam-track, hloc, hold_env). Includes commented aliases for metro and
    100doh.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -171,6 +171,15 @@ function setup_functions
         end
     end
 
+    function load_hold_commands
+        alias pyhamer='~/third_party_models/hold/generator/hamer/.pixi/envs/default/bin/python'
+        alias pysam='~/third_party_models/hold/generator/sam-track/.pixi/envs/default/bin/python'
+        alias pycolmap='~/third_party_models/hold/generator/hloc/.pixi/envs/default/bin/python'
+        alias pyhold='~/third_party_models/hold/hold_env/.pixi/envs/default/bin/python'
+        # alias pymetro='~/third_party_models/hold/generator/metro/.pixi/envs/default/bin/python'
+        # alias pydoh='~/third_party_models/hold/generator/100doh/.pixi/envs/default/bin/python'
+    end
+
     alias o="xdg_open2"
     function xdg_open2
         if test -n $IS_WSL
