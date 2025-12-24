e7859f0a2f0c8f9a21f49a3af90d36f1468c424f
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-12-24 15:14:30 -0800

    refactor(install): update dotfile links and cleanup
    - Removed .popt file and its linking from install.sh.
    - Reorganized and updated the list of dotfiles and configs to link:
      - Added links for .config/fish, .config/lsd, .config/starship.toml,
        .config/zathura, .config/zellij/config.kdl, .fzf.bash, .fzf.zsh,
        .latexmkrc, .pixi/manifests/pixi-global.toml, .tmux.conf, .zshrc.
      - Commented out or removed links for non-portable or unused configs
        (e.g., karabiner, sheldon, spacemacs, wezterm.lua).
    - Improved comments for clarity and maintainability.

--- install.sh
+++ install.sh
@@ -22,30 +22,26 @@ function link_to_home() {
     echo
 }
 
-link_to_home .tmux.conf
-link_to_home .latexmkrc
-link_to_home .zshrc
+# link_to_home .config/karabiner/karabiner.json # karabiner settings are not portable; connected mouses and keyboards matter
+# link_to_home .config/sheldon
+# link_to_home .spacemacs
+# link_to_home .wezterm.lua
+link_to_home .config/fish
+link_to_home .config/git
+link_to_home .config/kitty
+link_to_home .config/lsd
+link_to_home .config/neovide
+link_to_home .config/starship.toml
+link_to_home .config/zathura
+link_to_home .config/zellij/config.kdl
 link_to_home .fzf.bash
 link_to_home .fzf.zsh
-link_to_home .wezterm.lua
-link_to_home .config/starship.toml
-link_to_home .config/sheldon
-link_to_home .config/lsd
-# karabiner settings are not portable; connected mouses and keyboards matter
-# mkdir -p ~/.config/karabiner
-# link_to_home .config/karabiner/karabiner.json
-link_to_home .config/git
-link_to_home .config/zathura
-link_to_home .config/kitty
-link_to_home .config/neovide
-link_to_home .config/fish
-link_to_home .rsyncignore
-link_to_home .wezterm.lua
-link_to_home .popt
-link_to_home .spacemacs
-link_to_home scripts/ssync
+link_to_home .latexmkrc
 link_to_home .pixi/manifests/pixi-global.toml
-link_to_home .config/zellij/config.kdl
+link_to_home .rsyncignore
+link_to_home .tmux.conf
+link_to_home .zshrc
+link_to_home scripts/ssync
 
 # If linux, link fcitx5 hotkey extension
 # To use this, you need to run: `yay -S fcitx5-lua`
