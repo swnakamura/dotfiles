905bfc43dac766dd2830c357409fc14ee4cbe196
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2026-04-02 04:57:28 +0900

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -286,7 +286,7 @@ require('lazy').setup({
           enabled = true,
           preset = {
             keys = {
-              { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+              { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.picker.files({ layout = 'telescope', hidden = true, ignored = true })" },
               { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
               { icon = "󰦨 ", key = "G", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
               { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
