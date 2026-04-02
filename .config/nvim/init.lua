3bd99b71d53388cfb61f47ace1c69fd893b5d298
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2026-04-02 19:18:59 +0900

    neovim: remove unused aerial config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2246,12 +2246,6 @@ require('lazy').setup({
     'stevearc/aerial.nvim',
     config = function()
       require("aerial").setup({
-        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-        on_attach = function(bufnr)
-          -- Jump forwards/backwards with '{' and '}'
-          -- vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-          -- vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-        end,
         close_on_select = true,
         autojump = true,
       })
